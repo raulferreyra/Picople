2eba62756e65e640451de1a218c4f5a47ede63d1
Raúl Ferreyra <raulrferreyrav@gmail.com>
2025-08-16 22:18:15 -0500

    Update init

--- src/picople/app/controllers/__init__.py
+++ src/picople/app/controllers/__init__.py
@@ -1,5 +1,8 @@
 from .MediaListModel import MediaListModel
 from .SystemProbe import SystemProbe
 from .ProbeResult import ProbeResult
+from .MediaItem import MediaItem
+from .MediaNavigator import MediaNavigator
 
-__all__ = ["MediaListModel", "SystemProbe", "ProbeResult"]
+__all__ = ["MediaListModel", "SystemProbe",
+           "ProbeResult", "MediaItem", "MediaNavigator"]
